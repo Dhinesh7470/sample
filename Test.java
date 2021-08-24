b2804e9efd6436b920b1c14899f8b832554c4a7d
Dhinesh7470 <dhineshjp747@gmail.com>
2021-08-24 11:58:28 +0530

    url done

--- Test.java
+++ Test.java
@@ -1,5 +1,13 @@
 package org.smp;
 
 public class Test {
+	
+	public void url() {
+		System.out.println("https://en-gb.facebook.com/");
+	}
+	public static void main(String[] args) {
+		Test t=new Test();
+		t.url();
+	}
 
 }
